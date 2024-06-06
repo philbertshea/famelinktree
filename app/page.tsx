7fdd0e4691defd0cff367fdab494f0601b50f04e
Philbert <philbertshea@gmail.com>
2024-06-06 15:58:04 +0800

    Added forms and files

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,23 @@ import Image from "next/image";
 import data from "../data.json";
 
 function LinkCard({ href, title, image }: {href: string, title: string, image?: string}) {
-  return (
-    <a 
-    href={href} 
-    className="flex items-center p-4 w-full rounded-md scale-90 text-black hover:text-white hover:scale-95 hover:bg-sky-700 transition-all border border-orange-600 bg-sky-300 mb-3">
-      <div className="flex flex-col text-center">
-        <h2 className="font-bold text-lg">{title}</h2>
-      </div>
-    </a>
-  );
+  if (href === "") {
+      return (
+        <div className="flex flex-col text-center">
+            <h2 className="font-bold text-lg">{title}</h2>
+        </div>
+      );
+  } else {
+      return (
+        <a 
+          href={href} 
+          className="flex items-center p-4 w-full rounded-md scale-90 text-black hover:text-white hover:scale-95 hover:bg-sky-700 transition-all border border-orange-600 bg-sky-300 mb-3">
+          <div className="flex flex-col text-center">
+            <h2 className="font-bold text-xl">{title}</h2>
+          </div>
+        </a>
+      );
+  }
 }
 
 export default function Home() {
@@ -25,6 +33,7 @@ export default function Home() {
           height="200"
           />
         <h1 className="font-bold mt-4 text-xl">{data.name}</h1>
+        <h2 className="text-m mt-5 mb-5 pl-56 pr-56">{data.description}</h2>
           {data.links.map((link) => (
             <LinkCard key={link.url} href={link.url} {...link} />
           ))}
